8e5f2fe370ca7b61e415529d6bec6c5dcec5f9e6
Nikolay Todorov <everflowing555@gmail.com>
2026-09-09 19:01:49 +0300

    feat: added ssh NSG rule for gateway VM

--- infra/modules/networking/nsg.tf
+++ infra/modules/networking/nsg.tf
@@ -10,7 +10,7 @@ resource "azurerm_network_security_group" "gateway" {
     access                     = "Allow"
     protocol                   = "Tcp"
     source_port_range          = "*"
-    destination_port_range     = "22"
+    destination_port_range     = "2222"
     source_address_prefix      = var.admin_ip
     destination_address_prefix = "*"
   }
